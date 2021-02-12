a0972ad3de8506b03467bcbb4f473a17f97d62af
Aid Thompson <thepandariot@gmail.com>
2021-02-12 22:38:53 +0000

    added hover fx in JS to fade in out the bg img

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ function selectVenue(id, town, price) {
   console.log(id, town, " and price is ", price)
 }
 
-function mouseHasEntered() {
-  console.log("Mouse Has ENtered!")
-  const image = document.getElementById("hover-fx-img-id");
-  const fadeToWhite = document.getElementsByClassName("fade-to-white");
-  for(var i = 0; i < fadeToWhite.length; i++) {
-    fadeToWhite[i].style.color = "white";
-  }
-  image.style.opacity = 1;
+function mouseHasEntered(ref) {
+  console.log("Mouse Has Entered!", ref);
+  // const image = document.getElementById("hover-fx-img-id");
+  // const fadeToWhite = document.getElementsByClassName("fade-to-white");
+  // for(var i = 0; i < fadeToWhite.length; i++) {
+  //   fadeToWhite[i].style.color = "white";
+  // }
+  ref.style.opacity = 1;
 }
 
-function mouseHasLeft() {
+function mouseHasLeft(ref) {
   console.log("mouse has left!");
-  const image = document.getElementById("hover-fx-img-id");
-  image.style.opacity = 0.3;
-  const fadeToWhite = document.getElementsByClassName("fade-to-white");
-  for(var i = 0; i < fadeToWhite.length; i++) {
-    fadeToWhite[i].style.color = "black";
-  }
+  // const image = document.getElementById("hover-fx-img-id");
+  // image.style.opacity = 0.3;
+  // const fadeToWhite = document.getElementsByClassName("fade-to-white");
+  // for(var i = 0; i < fadeToWhite.length; i++) {
+  //   fadeToWhite[i].style.color = "black";
+  // }
+  ref.style.opacity = 0.3;
 }
 
