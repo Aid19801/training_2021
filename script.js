e6fd1e7aebafd6dc3127950db74c388d51d92e79
Aid Thompson <thepandariot@gmail.com>
2021-02-13 12:15:48 +0000

    reffy way of fixing css js hover effect

--- script.js
+++ script.js
@@ -6,23 +6,18 @@ function selectVenue(id, town, price) {
 }
 
 function mouseHasEntered(ref) {
-  console.log("Mouse Has Entered!", ref);
-  // const image = document.getElementById("hover-fx-img-id");
-  // const fadeToWhite = document.getElementsByClassName("fade-to-white");
-  // for(var i = 0; i < fadeToWhite.length; i++) {
-  //   fadeToWhite[i].style.color = "white";
-  // }
-  ref.style.opacity = 1;
+  console.log("Mouse Has Entered!");
+  const venueImg = ref.children[0];
+  const venueDetailsDiv = ref.children[1];
+  venueDetailsDiv.style.opacity = 1;
+  venueImg.style.opacity = 1;
 }
 
 function mouseHasLeft(ref) {
   console.log("mouse has left!");
-  // const image = document.getElementById("hover-fx-img-id");
-  // image.style.opacity = 0.3;
-  // const fadeToWhite = document.getElementsByClassName("fade-to-white");
-  // for(var i = 0; i < fadeToWhite.length; i++) {
-  //   fadeToWhite[i].style.color = "black";
-  // }
-  ref.style.opacity = 0.3;
+  const venueImg = ref.children[0];
+  const venueDetailsDiv = ref.children[1];
+  venueDetailsDiv.style.opacity = 0.3;
+  venueImg.style.opacity = 0.3;
 }
 
